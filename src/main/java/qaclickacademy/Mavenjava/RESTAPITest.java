db279856f20d25239798bbc19a5f8f277f02a496
Atman Mehta <B003240@federated.fds>
2019-09-30 10:00:10 -0400

    First commit

---
+++ src/main/java/qaclickacademy/Mavenjava/RESTAPITest.java
@@ -0,0 +1,22 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void postjira()
+	
+	{
+		System.out.println("RESTAPITest");
+	}
+
+	
+	@Test
+	public void deleteTwitter()
+	
+	{
+		System.out.println("deleteTwitter");
+	}
+	
+}
